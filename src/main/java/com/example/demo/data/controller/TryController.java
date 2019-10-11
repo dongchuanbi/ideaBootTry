de84c67834078d3aa014b1ceb78b6dea7b31e17b
dongchuanbi <dongchuanbi@126.com>
2019-10-11 09:17:45 +0800

    try

--- src/main/java/com/example/demo/data/controller/TryController.java
+++ src/main/java/com/example/demo/data/controller/TryController.java
@@ -4,12 +4,14 @@ package com.example.demo.data.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/try")
 public class TryController {
 
 	@GetMapping("/tryDemo")
 	public String tryDemo(){
-		return "给你demo啊！!";
+		String str="给你demo啊！!!";
+		return str;
 	}
 }
